cb8ded65add3e67108183d1ab5e42f6ee503674a
ExtremeBoyGG <herilrtg123@gmail.com>
2026-03-15 20:22:11 +0700

    fix: Match hexated Gradle config
    - Use Gradle 7.4 (was 8.5)
    - Use Android Gradle Plugin 7.0.4 (was 8.2.2)
    - Match gradle.properties format
    - Fix: Unresolved reference: cloudstream

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("com.android.tools.build:gradle:7.0.4")
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
