516f63338498559ff17abfd3c0b035b8042b181d
ExtremeBoyGG <herilrtg123@gmail.com>
2026-03-16 02:33:08 +0700

    Pemangkasan fitur dikit

--- Oploverz/src/main/kotlin/com/indo/Oploverz.kt
+++ Oploverz/src/main/kotlin/com/indo/Oploverz.kt
@@ -4,6 +4,7 @@ import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.LoadResponse.Companion.addAniListId
 import com.lagradost.cloudstream3.LoadResponse.Companion.addMalId
 import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.newExtractorLink
 import com.lagradost.cloudstream3.utils.loadExtractor
 
 class Oploverz : MainAPI() {
@@ -155,14 +156,14 @@ class Oploverz : MainAPI() {
             .find(decoded)?.value ?: return
 
         callback.invoke(
-            ExtractorLink(
+            newExtractorLink(
                 source = "Blogger",
                 name = "Blogger",
                 url = videoUrl,
-                referer = "https://www.blogger.com/",
-                quality = com.lagradost.cloudstream3.utils.Qualities.Unknown.value,
-                isM3u8 = false
-            )
+                referer = "https://www.blogger.com/"
+            ) {
+                this.quality = com.lagradost.cloudstream3.utils.Qualities.Unknown.value
+            }
         )
     }
 }
