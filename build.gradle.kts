ea81c4ce1ed82a26040c0e184ceaea0e6c208600
ExtremeBoyGG <herilrtg123@gmail.com>
2026-03-15 19:30:03 +0700

    feat: Add root build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+// Top-level build file
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
