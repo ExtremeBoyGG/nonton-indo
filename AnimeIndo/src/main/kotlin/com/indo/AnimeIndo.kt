63dfc866b0a17cb2e0549bfeb4420d1f3504df84
ExtremeBoyGG <herilrtg123@gmail.com>
2026-03-16 15:20:15 +0700

    fix: add missing Qualities import

--- AnimeIndo/src/main/kotlin/com/indo/AnimeIndo.kt
+++ AnimeIndo/src/main/kotlin/com/indo/AnimeIndo.kt
@@ -5,6 +5,7 @@ import com.lagradost.cloudstream3.LoadResponse.Companion.addAniListId
 import com.lagradost.cloudstream3.LoadResponse.Companion.addMalId
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.loadExtractor
+import com.lagradost.cloudstream3.utils.Qualities
 
 class AnimeIndo : MainAPI() {
     override var mainUrl = "https://anime-indo.lol"
